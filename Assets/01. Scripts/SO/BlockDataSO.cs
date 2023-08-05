b65276fc96a09f9fd735c9435790f52460c77eca
SEH00N <06psh0501@gmail.com>
2023-08-06 03:30:43 +0900

    block building

--- Assets/01. Scripts/SO/BlockDataSO.cs
+++ Assets/01. Scripts/SO/BlockDataSO.cs
@@ -5,6 +5,8 @@ public class BlockDataSO : ScriptableObject
 {
     public LayerMask targetLayer;
     public Sprite blockSprite;
+    public PolygonCollider2D polygon;
+    public string blockName;
     public float maxHP;
     public float damage;
 }
